3e8cbdf3e4dd83eec5f6114e3d5f5edcd1e0bd61
fajfara <a.fajfar95@gmail.com>
2018-05-13 18:55:34 +0200

    Uploading files

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const subtitleFirst = document.getElementById("item-1-subtitle");
+const image1 = document.getElementById("image-1");
+
+function changeSubtitleFirst() {
+    subtitleFirst.innerHTML = "www.test.com";
+    image1.classList.add("move-from-left");
+
+}
+
+function changeFirstDefault() {
+    subtitleFirst.innerHTML = "Description of the project";
+    image1.classList.remove("move-from-left");
+}
