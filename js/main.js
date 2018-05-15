ad9461a7f638167c2e478f4ffbe2f2281d04de8b
fajfara <a.fajfar95@gmail.com>
2018-05-15 17:28:42 +0200

    Changed to css show on hover

--- js/main.js
+++ js/main.js
@@ -1,14 +1,2 @@
-const subtitleFirst = document.getElementById("item-1-subtitle");
-const image1 = document.getElementById("image-1");
 
-function changeSubtitleFirst() {
-    subtitleFirst.innerHTML = "www.test.com";
-    image1.classList.add("move-from-left");
 
-}
-
-function firsItemOff() {
-    subtitleFirst.innerHTML = "Description of the project";
-    image1.classList.remove("move-from-left");
-
-}
