f68bdc640cf193d12fec21d955b686fe52de61e8
fajfara <a.fajfar95@gmail.com>
2018-05-16 17:14:13 +0200

    Added nav and landing part of the page

--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
 
+const overlay = document.getElementById("overlay");
 
+window.onload = function() {
+
+}
+
+function openNav() {
+    overlay.classList.add("open");
+}
+
+function closeNav() {
+    overlay.classList.remove("open");
+}
